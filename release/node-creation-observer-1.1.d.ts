7d4ced389cc3b3062bd5b447f9fe7b251ab8a1a7
Soufiane Sakhi <soufiane.sakhi.js@gmail.com>
2016-03-27 23:02:48 +0200

    Env: Add Typescript definition file

---
+++ release/node-creation-observer-1.1.d.ts
@@ -0,0 +1,28 @@
+/**
+ * https://github.com/soufianesakhi/node-creation-observer-js
+ * MIT licensed
+ * Copyright (c) 2016 Soufiane Sakhi
+ */
+
+interface NodeCreationObserverStatic {
+    /**
+     * Add a new callback for a selector. The created element will be passed as the first parameter of the callback
+     */
+    onCreation: (selector: string, callback: (element: Node) => void, removeOnFirstMatch?: boolean) => void;
+    
+    /**
+     * Stop observing a selector
+     */
+    remove: (selector: string) => void,
+    
+    /**
+     * Stop observing all the selectors
+     */
+    stop: () => void;
+}
+
+declare module "node-creation-observer" {
+    export = NodeCreationObserver;
+}
+
+declare var NodeCreationObserver: NodeCreationObserverStatic;
